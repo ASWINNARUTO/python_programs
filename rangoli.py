0e55581528105f1523934c5859d8feb858ca3152
ASWINNARUTO <30527542+ASWINNARUTO@users.noreply.github.com>
2017-12-02 17:46:15 +0530

    Add files via upload

---
+++ rangoli.py
@@ -0,0 +1,12 @@
+import string
+def print_rangoli(size):
+    l=list(string.ascii_lowercase)
+    n=len("-".join(l[size-1:0:-1]+l[0:size:]))
+    for i in range(size-1,-1,-1):
+        print("-".join(l[size-1:i:-1]+l[i:size:]).center(n,"-"))
+    for i in range(1,size):
+        print("-".join(l[size-1:i:-1]+l[i:size:]).center(n,"-"))
+
+size=int(input())
+print_rangoli(size)
+        
